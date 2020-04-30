eb6180de2d5ccd40b41dc2ae9a46c7f143db01eb
Amin Arria <arria.amin@gmail.com>
2020-04-30 00:17:44 -0300

    Keep track of multiple subscriptions

--- assets/js/room.js
+++ assets/js/room.js
@@ -13,14 +13,14 @@ function subscribeToDj() {
     }));
 }
 
-function subscribeTo(publisher) {
+function subscribeTo() {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/api/party/subscribe", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({
         room_name: room_name,
         user: identity,
-        publisher: publisher
+        subscriptions: subscriptions
     }));
 }
 
@@ -42,37 +42,6 @@ function selfDisconnect(room) {
         });
     });
 }
-
-function insertParticipantOld(participant) {
-    console.log(participant.identity + ' joined the Room');
-    const party_members = document.getElementById('party-members');
-
-    new_member_li = document.createElement("li");
-    new_member_li.id = participant.identity;
-
-    text = document.createTextNode(participant.identity);
-    new_member_li.appendChild(text);
-
-    join_button = document.createElement("button")
-
-    participant.on('trackPublished', () => {
-        if(join_button.childNodes.length == 0) {
-            join_button.addEventListener("click", () => {
-                subscribeTo(participant.identity);
-            });
-            join_button_text = document.createTextNode("Watch");
-            join_button.appendChild(join_button_text);
-            new_member_li.appendChild(join_button);
-        }
-    });
-
-    participant.on('trackUnpublished', () => {
-        join_button.remove();
-    });
-
-    party_members.appendChild(new_member_li);
-}
-
 
 function insertParticipant(participant) {
     console.log(participant.identity + ' joined the Room');
@@ -102,7 +71,9 @@ function insertParticipant(participant) {
             watch_button.classList.remove("hidden");
 
             watch_button.addEventListener("click", () => {
-                subscribeTo(participant.identity);
+                subscriptions.push({"type": "include", "publisher": participant.identity});
+                subscribeTo();
+                // subscribeTo(participant.identity);
                 watch_button.classList.add("hidden");
                 unwatch_button.classList.remove("hidden");
             });
@@ -273,6 +244,8 @@ document.addEventListener("DOMContentLoaded", function() {
     start_share = document.getElementById("start-share");
     stop_share = document.getElementById("stop-share");
     party = document.getElementById("party");
+
+    subscriptions = [];
 
     if (start_party) {
         start_party.addEventListener("submit", connectToParty);
